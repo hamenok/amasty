7c6ecbadbddfe5b1f4bd3ea62fbecdd280f59643
Hamenok <Zdrapalo@gmail.com>
2022-08-04 00:48:00 +0300

    Task-1 with additional task

--- Hamenok/Controller/Index/Index.php
+++ Hamenok/Controller/Index/Index.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+
 namespace Amasty\Hamenok\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
 
-/**
- * Catalog index page controller.
- */
 class Index extends \Magento\Framework\App\Action\Action implements HttpGetActionInterface
 {
     /**
